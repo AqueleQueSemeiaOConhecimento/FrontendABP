3d8e743f431d79b658844c7f6ecacb502d7a5a79
Matheus <D3rr3tido@gmail.com>
2025-05-19 13:10:32 -0300

    arrumando esquema de rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/elements/header/header.component';
+import { FooterComponent } from './components/elements/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
